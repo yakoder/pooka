2e9f3372b3a130482bf3e045267a88229e5a2eaf
jphekman <jphekman@cf05c91d-1d0e-0410-8b41-cf3121da992e>
2000-01-17 03:05:08 +0000

    *** empty log message ***

---
+++ CVSROOT/pooka/net/suberic/util/gui/PasswordEditorPane.java
@@ -0,0 +1,57 @@
+package net.suberic.util.gui;
+import javax.swing.*;
+import net.suberic.util.*;
+import java.awt.FlowLayout;
+
+public class PasswordEditorPane extends DefaultPropertyEditor {
+    String property;
+    String originalValue;
+    JLabel label;
+    JPasswordField inputField;
+    VariableBundle sourceBundle;
+
+    public PasswordEditorPane(String newProperty, VariableBundle bundle, boolean isEnabled) {
+	property=newProperty;
+	sourceBundle=bundle;
+	originalValue = sourceBundle.getProperty(newProperty, "");
+
+	String defaultLabel;
+	int dotIndex = property.lastIndexOf(".");
+	if (dotIndex == -1) 
+	    defaultLabel = new String(property);
+	else
+	    defaultLabel = property.substring(dotIndex+1);
+
+	label = new JLabel(sourceBundle.getProperty(property + ".label", defaultLabel));
+	inputField = new JPasswordField(originalValue);
+	this.add(label);
+	this.add(inputField);
+	this.setEnabled(isEnabled);
+    }
+
+    public PasswordEditorPane(String newProperty, VariableBundle bundle) {
+	this(newProperty, bundle, true);
+    }
+
+    public void setValue() {
+	if (isEnabled() && !(inputField.getText().equals(originalValue)))
+	sourceBundle.setProperty(property, inputField.getText());
+    }
+
+    public java.util.Properties getValue() {
+	java.util.Properties retProps = new java.util.Properties();
+	retProps.setProperty(property, inputField.getText());
+	return retProps;
+    }
+
+    public void resetDefaultValue() {
+	inputField.setText(originalValue);
+    }
+
+    public void setEnabled(boolean newValue) {
+	if (inputField != null) {
+	    inputField.setEnabled(newValue);
+	    enabled=newValue;
+	}
+    }
+}
