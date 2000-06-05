19c3947a474438215f898c32f1d18a661ceff7af
allen <allen@cf05c91d-1d0e-0410-8b41-cf3121da992e>
2000-06-05 19:17:20 +0000

    updated ExternalLauncher so that it will correctly take a setting to remove the temporary file on exit of the outside program.
    also updated Pookarc to reflect this.
    
    note that even on systems where this feature isn't used, external program
    _should_ still start up, and the temporary file _should_ be deleted, though
    on Pooka exit rather than on external program exit.

--- pooka/net/suberic/pooka/ExternalLauncher.java
+++ pooka/net/suberic/pooka/ExternalLauncher.java
@@ -2,7 +2,12 @@ package net.suberic.pooka;
 
 import javax.activation.*;
 import java.io.*;
+import java.util.StringTokenizer;
 
+/**
+ * This class is a generic class which will allow an external program
+ * to start up and access a file.
+ */
 public class ExternalLauncher implements CommandObject, Runnable {
 
     private String verb;
@@ -11,6 +16,14 @@ public class ExternalLauncher implements CommandObject, Runnable {
     public ExternalLauncher() {
     };
 
+    /**
+     * This sets the CommandContext to the given command and DataHandler.
+     * Note that for this implementation, the verb is expected to bet the
+     * external command which is run, with %s representing the name of
+     * the temporary file.
+     *
+     * As specified in javax.activation.CommandObject.
+     */
     public void setCommandContext(java.lang.String newVerb,
 				  DataHandler newDh)
 	throws java.io.IOException {
@@ -18,50 +31,88 @@ public class ExternalLauncher implements CommandObject, Runnable {
 	dh = newDh;
     }
 
+    /**
+     * This starts the run() method in a separate Thread.  It is implemented
+     * this way so as to present the same interface as a CommandObject which
+     * extends Window.
+     */
     public void show() {
 	Thread t = new Thread(this, "External Viewer");
 	t.run();
     }
 
+    /**
+     * This is the main method for the ExternalLaucher.  It creates a 
+     * temporary file from the DataHandler and then uses the verb command,
+     * along with the wrappers specified by ExternalLauncher.fileHandler
+     * and ExternalLauncher.cmdWrapper, to access the file itself.
+     *
+     * As specified by java.lang.Runnable.
+     */
     public void run() {
 	try {
 	    File tmpFile = File.createTempFile("pooka", ".tmp");
 	    FileOutputStream fos = new FileOutputStream(tmpFile);
 	    dh.writeTo(fos);
 	    
+	    String fileHandler = Pooka.getProperty("ExternalLauncher.fileHandler." + java.io.File.separator, null);
 	    String wrapper = Pooka.getProperty("ExternalLauncher.cmdWrapper." + java.io.File.separator, null);
-	    if (wrapper != null) 
-		verb = insertWrapper(verb, wrapper);
-	    
-	    StringBuffer cmd = new StringBuffer(verb);		
-		
 	    String fileName = tmpFile.getAbsolutePath();
-	    
-	    int currentFile = verb.lastIndexOf("%s", verb.length());
-	    while (currentFile != -1) {
-		cmd.replace(currentFile, currentFile +2, fileName);
-		currentFile = verb.lastIndexOf("%s", currentFile -1);
+
+	    String parsedVerb;
+	    String[] cmdArray;
+
+	    if (fileHandler != null && wrapper != null) { 
+
+		parsedVerb = substituteString(fileHandler, "%v", verb);
+		parsedVerb = substituteString(parsedVerb, "%s", fileName);
+		
+		System.out.println("wrapper is " + wrapper);
+		StringTokenizer tok = new StringTokenizer(wrapper);
+		cmdArray = new String[tok.countTokens()];
+		for (int i = 0; tok.hasMoreTokens(); i++) {
+		    String currentString = tok.nextToken();
+		    if (currentString.equals("%v"))
+			cmdArray[i] = parsedVerb;
+		    else
+			cmdArray[i]=currentString;
+		}
+	    } else {
+		parsedVerb = substituteString(verb, "%s", fileName);
+		
+		StringTokenizer tok = new StringTokenizer(wrapper);
+		cmdArray = new String[tok.countTokens()];
+		for (int i = 0; tok.hasMoreTokens(); i++) {
+		    String currentString = tok.nextToken();
+		    cmdArray[i]=tok.nextToken();
+		}
+		
+		tmpFile.deleteOnExit();
 	    }
-	    
-	    System.out.println("running command " + cmd.toString());
-	    Runtime.getRuntime().exec(cmd.toString());
+
+	    Runtime.getRuntime().exec(cmdArray);
+
 	} catch (java.io.IOException ioe) {
 	    System.out.println("Error opening temp file " + ioe.getMessage());
 	}
     }
     
-    public String insertWrapper(String origCmd, String cmdWrapper) {
-	System.out.println("inserting wrapper " + cmdWrapper + " on " + origCmd);
-	StringBuffer modifiedCmd = new StringBuffer(cmdWrapper);
-	int currentCmd = cmdWrapper.lastIndexOf("%v", cmdWrapper.length());
-	while (currentCmd != -1) {
-	    modifiedCmd.replace(currentCmd, currentCmd +2, origCmd);
-	    currentCmd = cmdWrapper.lastIndexOf("%v", currentCmd -1);
+    /**
+     * This method subsitutes all occurances of key with value in String
+     * original.
+     */
+    public String substituteString(String original, String key, String value) {
+	// you know, i'm already doing this for the replyIntro; maybe i
+	// should generalize these both a bit...
+
+	StringBuffer modifiedString = new StringBuffer(original);
+	int current = original.lastIndexOf(key, original.length());
+	while (current != -1) {
+	    modifiedString.replace(current, current + key.length(), value);
+	    current = original.substring(0, current).lastIndexOf(key, current);
 	}
 	
-	System.out.println("returning " + modifiedCmd);
-	
-	return modifiedCmd.toString();
+	return modifiedString.toString();
     }
 }
 
