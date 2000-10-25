1ce1c92ed676c72edb4fee702590d67ea5b47714
allen <allen@cf05c91d-1d0e-0410-8b41-cf3121da992e>
2000-10-25 22:43:00 +0000

    trying to fix a display bug.

--- pooka/net/suberic/pooka/gui/ReadMessageDisplayPanel.java
+++ pooka/net/suberic/pooka/gui/ReadMessageDisplayPanel.java
@@ -158,16 +158,27 @@ public class ReadMessageDisplayPanel extends MessageDisplayPanel {
      * default value.
      */
     public void sizeToDefault() {
-	Dimension prefSize = getDefaultEditorPaneSize();
-	JScrollBar vsb = editorScrollPane.getVerticalScrollBar();
-	if (vsb != null)
-	    prefSize.setSize(prefSize.getWidth() + vsb.getPreferredSize().getWidth(), prefSize.getHeight());
-	editorScrollPane.setPreferredSize(prefSize);
-	int width = prefSize.width;
-	this.setPreferredSize(new Dimension(width, width));
-	this.setSize(this.getPreferredSize());
-	if (splitPane != null && attachmentPanel != null)
-	    splitPane.setDividerLocation((int)(splitPane.getSize().getHeight() - attachmentPanel.getPreferredSize().getHeight()));
+    }
+
+    public void addNotify() {
+	super.addNotify();
+	
+	if (firstShow) {
+	    Dimension prefSize = getDefaultEditorPaneSize();
+	    JScrollBar vsb = editorScrollPane.getVerticalScrollBar();
+	    if (vsb != null)
+		prefSize.setSize(prefSize.getWidth() + vsb.getPreferredSize().getWidth(), prefSize.getHeight());
+	    editorScrollPane.setPreferredSize(prefSize);
+	    int width = prefSize.width;
+	    this.setPreferredSize(new Dimension(width, width));
+	    this.setSize(this.getPreferredSize());
+	    if (splitPane != null && attachmentPanel != null) {
+		System.out.println("setting splitPane divider location to " + (splitPane.getPreferredSize().getHeight() - attachmentPanel.getPreferredSize().getHeight()));
+		splitPane.setDividerLocation((int)(splitPane.getPreferredSize().getHeight() - attachmentPanel.getPreferredSize().getHeight()));
+	    }
+	    
+	    firstShow = false;
+	}
     }
     
 
