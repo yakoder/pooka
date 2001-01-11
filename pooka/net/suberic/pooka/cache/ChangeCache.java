76ce5a1e3992707b544d296d5ad43a986495189a
allen <allen@cf05c91d-1d0e-0410-8b41-cf3121da992e>
2001-01-11 20:51:54 +0000

    updated todo file.
    added ChangeCache (a terrible name; i might change it), which is a class
    that saves flag changes and expunge operations and then sends them out to
    a folder at a later time.

---
+++ pooka/net/suberic/pooka/cache/ChangeCache.java
@@ -0,0 +1,223 @@
+package net.suberic.pooka.cache;
+import javax.mail.*;
+import java.io.*;
+
+/**
+ * A utility class that saves the changes made to messages into a file.
+ * Then when we reconnect to the server, we can make all of the changes
+ * in batch.
+ */
+public class ChangeCache {
+
+    File cacheFile = null;
+    File cacheDir = null;
+    File tmpFile = null;
+
+    public static String EXPUNGE_MSG = "FOLDER EXPUNGE";
+    public static String DONE_MSG = "INPUT DONE";
+
+    boolean lock = false;
+
+    /**
+     * Creates a new ChangeCache object based out of the given directory.
+     */
+    public ChangeCache(File dir) {
+	cacheDir = dir;
+	cacheFile = new File(dir, "changeLog");
+    }
+
+    /**
+     * Opens the cache file.  Actually creates a temporary file, copies the
+     * contents of the original cache file into there, and returns a writer
+     * to that file.
+     */
+    private BufferedWriter openCacheFile() throws IOException {
+	boolean hasLock = false;
+	while (! hasLock) {
+	    synchronized(this) {
+		if (! lock ) {
+		    lock = true;
+		    hasLock = true;
+		}
+	    }
+
+	    if (! hasLock )
+		try {
+		    Thread.sleep(1000);
+		} catch (Exception e) { }
+	}
+	tmpFile = new File(cacheDir, "changeLog.tmp");
+	
+	// if there's a temp file, assume it's an old one left
+	// over from before.
+	if (tmpFile.exists())
+	    tmpFile.delete();
+	tmpFile.createNewFile();
+	
+	BufferedWriter out = new BufferedWriter(new FileWriter(tmpFile));
+	if (cacheFile.exists()) {
+	    BufferedReader in = new BufferedReader(new FileReader(cacheFile));
+	    String nextLine = in.readLine();
+	    while (nextLine != null) {
+		out.write(nextLine);
+		out.newLine();
+	    }
+	    
+	    in.close();
+	}
+	return out;
+    }
+
+    private void closeCacheFile(BufferedWriter out) throws IOException {
+	out.flush();
+	out.close();
+	
+	if (cacheFile.exists())
+	    cacheFile.delete();
+	
+	tmpFile.renameTo(cacheFile);
+	tmpFile = null;
+
+	lock = false;
+    }
+
+    /**
+     * Writes out the given flags to be set to value value on the message with
+     * the given uid.
+     */
+    public void setFlags(long uid, Flags f, boolean value) throws IOException {
+	BufferedWriter out = openCacheFile();
+
+	out.write(Long.toString(uid));
+	out.newLine();
+
+	Flags.Flag[] systemFlags = f.getSystemFlags();
+	for (int i = 0; i < systemFlags.length; i++) {
+	    if (systemFlags[i] == Flags.Flag.ANSWERED) {
+		out.write("Answered");
+		out.newLine();
+	    } else if (systemFlags[i] == Flags.Flag.DELETED) {
+		out.write("Deleted");
+		out.newLine();
+	    } else if (systemFlags[i] == Flags.Flag.DRAFT) {
+		out.write("Draft");
+		out.newLine();
+	    } else if (systemFlags[i] == Flags.Flag.FLAGGED) {
+		out.write("Flagged");
+		out.newLine();
+	    } else if (systemFlags[i] == Flags.Flag.RECENT) {
+		out.write("Recent");
+		out.newLine();
+	    } else if (systemFlags[i] == Flags.Flag.SEEN) {
+		out.write("Seen");
+		out.newLine();
+	    }
+
+	}
+	    
+	String[] userFlags = f.getUserFlags();
+	for (int i = 0; i < userFlags.length; i++) {
+	    out.write(userFlags[i]);
+	    out.newLine();
+	}
+
+	if (value) 
+	    out.write("true");
+	else
+	    out.write("false");
+	
+	out.newLine();
+	    
+	out.write(DONE_MSG);
+	out.newLine();
+
+	closeCacheFile(out);
+	
+    }
+	
+    public void expunge() throws IOException {
+	BufferedWriter out = openCacheFile();
+
+	out.write(EXPUNGE_MSG);
+	out.newLine();
+
+	closeCacheFile(out);
+    }
+
+    public void writeChanges(UIDFolder f) throws IOException, MessagingException {
+	boolean hasLock = false;
+	while (! hasLock) {
+	    synchronized(this) {
+		if (! lock ) {
+		    lock = true;
+		    hasLock = true;
+		}
+	    }
+	    
+	    if (! hasLock )
+		try {
+		    Thread.sleep(1000);
+		} catch (Exception e ) { }
+	}
+	
+	BufferedReader in = new BufferedReader(new FileReader(cacheFile));
+	String nextLine = in.readLine();
+	while (nextLine != null) {
+	    if (nextLine.equalsIgnoreCase(EXPUNGE_MSG))
+		try {
+		    ((Folder) f).expunge();
+		} catch (MessagingException me) { }
+	    else if (nextLine.length() > 0) {
+		// adding flags
+		
+		boolean value;
+		Flags newFlags = new Flags();
+
+		long uid = Long.parseLong(nextLine);
+		nextLine = in.readLine();
+		while (nextLine != null && ! nextLine.equals(DONE_MSG)) {
+		    
+		    if (nextLine.equalsIgnoreCase("Deleted"))
+			newFlags.add(Flags.Flag.DELETED);
+		    else if (nextLine.equalsIgnoreCase("Answered"))
+			newFlags.add(Flags.Flag.ANSWERED);
+		    else if (nextLine.equalsIgnoreCase("Draft"))
+			newFlags.add(Flags.Flag.DRAFT);
+		    else if (nextLine.equalsIgnoreCase("Flagged"))
+			newFlags.add(Flags.Flag.FLAGGED);
+		    else if (nextLine.equalsIgnoreCase("Recent"))
+			newFlags.add(Flags.Flag.RECENT);
+		    else if (nextLine.equalsIgnoreCase("SEEN"))
+			newFlags.add(Flags.Flag.SEEN);
+		    else 
+			newFlags.add(new Flags(nextLine));
+
+		    nextLine = in.readLine();
+		}
+
+		nextLine = in.readLine();
+		if (nextLine.equalsIgnoreCase("true"))
+		    value = true;
+		else
+		    value = false;
+
+		try {
+		    Message m = f.getMessageByUID(uid);
+		    if (m != null)
+			m.setFlags(newFlags, value);
+		    
+		    // should be a done
+		    nextLine = in.readLine(); 
+		} catch (MessagingException me) { }
+	    }
+	    
+	    nextLine = in.readLine();
+	}
+	
+	in.close();
+	cacheFile.delete();
+	
+	lock = false;
+    }
+}
+
