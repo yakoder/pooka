8bfd64161e8d3a08cbbbf02e1710364e58923e1b
allen <allen@cf05c91d-1d0e-0410-8b41-cf3121da992e>
2001-01-19 18:34:16 +0000

    removed a debugging statement.

--- pooka/net/suberic/pooka/StoreInfo.java
+++ pooka/net/suberic/pooka/StoreInfo.java
@@ -155,7 +155,6 @@ public class StoreInfo implements ValueChangeListener {
 		else if (Pooka.getProperty(getStoreProperty() + "." + newFolderName + ".cacheMessages", "false").equalsIgnoreCase("true"))
 		    childFolder = new net.suberic.pooka.cache.CachingFolderInfo(this, newFolderName);
 		else if (Pooka.getProperty(getStoreProperty() + ".protocol", "mbox").equalsIgnoreCase("imap")) {
-		    System.out.println("making a new UIDFolder.");
 		    childFolder = new UIDFolderInfo(this, newFolderName);
 		} else
 		    childFolder = new FolderInfo(this, newFolderName);
