8e23b53cd41923a6656922e967c757d8e9e9cc17
allen <allen@cf05c91d-1d0e-0410-8b41-cf3121da992e>
2001-02-05 21:54:24 +0000

    added some code.  trying to make an interactive search form still.  :)

--- CVSROOT/pooka/net/suberic/pooka/gui/search/SearchEntryPanel.java
+++ CVSROOT/pooka/net/suberic/pooka/gui/search/SearchEntryPanel.java
@@ -10,15 +10,30 @@ import javax.swing.*;
  */
 public class SearchEntryPanel extends JPanel {
 
-    // bottom panel
+    public static int AND = 0;
+    public static int OR = 1;
+
     JPanel conditionPanel;
-    JScrollPane conditionScrollPane;
-    SearchTermPane[] searchTerms;
+    JPanel entryPanel;
+    JScrollPane entryScrollPane;
+    Vector searchTerms;
+    SearchTermManager manager;
+
+    class SearchEntryPair {
+	SearchEntryForm form;
+	int type;
+
+	public SearchEntryPair(SearchEntryForm newForm, int newType) {
+	    form = newForm;
+	    type = newType;
+	}
+    }
 
     /**
      * Creates a new SearchTermPanel.
      */
-    public SearchTermPanel() {
+    public SearchTermPanel(SearchTermManager newManager) {
+	manager = newManager;
 	populatePanel();
     }
     
@@ -27,10 +42,51 @@ public class SearchEntryPanel extends JPanel {
      * the constructor.
      */
     public void populatePanel() {
+	entryPanel = new JPanel();
+	entryPanel.setLayout(new BoxLayout(entryPanel, BoxLayout.Y_AXIS));
+
+	addSearchEntryForm(AND);
+
+	entryScrollPane = new JScrollPane(entryPanel);
+	this.add(entryScrollPane);
+    }
+
+    /**
+     * This creates the conditionPanel.  This is always the bottom panel,
+     * and contains the and / or buttons.  Pressing one of these buttons
+     * will create a new SearchEntryForm.
+     */
+    private void createConditionPanel() {
+	JPanel jp = new JPanel();
+	jp.setLayout(new FlowLayout());
+	
+	JButton buttonOne = new JButton(Pooka.getProperty("Search.button.and.label", "And"));
+	buttonOne.addActionListener(new ActionListener() {
+		public void actionPerformed(ActionEvent e) {
+		    addSearchEntryForm(AND);
+		}
+	    });
+
+	JButton buttonTwo = new JButton(Pooka.getProperty("Search.button.or.label", "Or"));
+	buttonTwo.addActionListener(new ActionListener() {
+		public void actionPerformed(ActionEvent e) {
+		    addSearchEntryForm(OR);
+		}
+	    });
+
+	jp.add(buttonOne);
+	jp.add(buttonTwo);
+
+	conditionPanel=jp;
+    }
+
+    public void addSearchEntryForm(int type) {
+	SearchEntryForm sef = new SearchEntryForm(manager);
+	
+	searchTerms.add(new SearchEntryPair(sef, type));
 	
     }
-    
-    
+
     public SearchTerm getSearchTerm() {
 	
     }
