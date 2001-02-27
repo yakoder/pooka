07c0c7daf8a948fa4d1ccc2d1f816c1661f9642d
akp <akp@cf05c91d-1d0e-0410-8b41-cf3121da992e>
2001-02-27 00:47:38 +0000

    added getAllFolders() method to return all open folders which can hold messages.

--- pooka/net/suberic/pooka/FolderInfo.java
+++ pooka/net/suberic/pooka/FolderInfo.java
@@ -443,6 +443,27 @@ public class FolderInfo implements MessageCountListener, ValueChangeListener, Us
     }
 
     /**
+     * Gets all of the children folders of this FolderInfo which are
+     * both Open and can contain Messages.  The return value should include
+     * the current FolderInfo, if it is Open and can contain Messages.
+     */
+    public Vector getAllFolders() {
+	Vector returnValue = new Vector();
+	if (children != null) {
+	    for (int i = 0 ; i < children.size(); i++) 
+		returnValue.addAll(((FolderInfo) children.elementAt(i)).getAllFolders());
+	}
+	
+	try {
+	    if (isOpen() && ((getFolder().getType() & Folder.HOLDS_MESSAGES) != 0))
+		returnValue.add(this);
+	} catch (MessagingException me) {
+	}
+
+	return returnValue;
+    }
+
+    /**
      * Loads the column names and sizes.
      */
     protected FetchProfile createColumnInformation() {
--- pooka/net/suberic/pooka/StoreInfo.java
+++ pooka/net/suberic/pooka/StoreInfo.java
@@ -472,6 +472,19 @@ public class StoreInfo implements ValueChangeListener {
 	}
     }
 
+    /**
+     * Gets all of the children folders of this StoreInfo which are both
+     * Open and can contain Messages.
+     */
+    public Vector getAllFolders() {
+	Vector returnValue = new Vector();
+	Vector subFolders = getChildren();
+	for (int i = 0; i < subFolders.size(); i++) {
+	    returnValue.addAll(((FolderInfo) subFolders.elementAt(i)).getAllFolders());
+	}
+	return returnValue;
+    }
+
     // Accessor methods.
 
     public Store getStore() {
