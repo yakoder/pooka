6a912ceaf18d2ad62426b6a36df13ec21a5cdbb6
akp <akp@cf05c91d-1d0e-0410-8b41-cf3121da992e>
2001-05-17 19:25:30 +0000

    working around a bug in the 1.2 jdk where AbstractActions aren't cloned properly.

--- pooka/net/suberic/util/DynamicAbstractAction.java
+++ pooka/net/suberic/util/DynamicAbstractAction.java
@@ -3,28 +3,18 @@ import javax.swing.AbstractAction;
 import javax.swing.Action;
 
 public abstract class DynamicAbstractAction extends AbstractAction {
-	
-	//java.util.HashSet properties = new java.util.HashSet();
-
-	public DynamicAbstractAction() {
-		super();
-	}
-	
-	public DynamicAbstractAction(String cmd) {
-		super(cmd);
-	}
-	
-	public Action cloneDynamicAction() throws CloneNotSupportedException {
-		//DynamicAbstractAction newAction = new DynamicAbstractAction();
-		
-		return (Action)this.clone();
-	}
-
-	/*
-	public synchronized void putValue (String key, Object newValue) {
-		super.putValue(key, newValue);
-		properties.add(key);
-	}
-	*/
-	
+  
+  public DynamicAbstractAction() {
+    super();
+  }
+  
+  public DynamicAbstractAction(String cmd) {
+    super(cmd);
+  }
+  
+  public Action cloneDynamicAction() throws CloneNotSupportedException {
+    this.putValue("foo", "bar");
+    return (Action)this.clone();
+  }
+  
 }
--- pooka/net/suberic/util/thread/ActionWrapper.java
+++ pooka/net/suberic/util/thread/ActionWrapper.java
@@ -13,93 +13,95 @@ import java.awt.event.ActionEvent;
  * original thread.
  */
 public class ActionWrapper extends net.suberic.util.DynamicAbstractAction {
-	Action wrappedAction;
-	ActionThread thread;
+  Action wrappedAction;
+  ActionThread thread;
+
+  /**
+   * This creates a new ActionWrapper from an Action and a Thread.
+   */
+  public ActionWrapper(Action newWrappedAction, ActionThread newThread) {
+    super();
+    wrappedAction=newWrappedAction;
+    thread=newThread;
+  }
+  
+  /**
+   * This performs the wrapped Action on the configured Thread.
+   */
+  public void actionPerformed(ActionEvent e) {
+    thread.addToQueue(wrappedAction, e);
+  }
 	
-	/**
-	 * This creates a new ActionWrapper from an Action and a Thread.
-	 */
-	public ActionWrapper(Action newWrappedAction, ActionThread newThread) {
-		super();
-		wrappedAction=newWrappedAction;
-		thread=newThread;
+  /**
+   * This passes the call on to the wrapped Action.
+   */
+  public void addPropertyChangeListener(PropertyChangeListener listener) {
+    wrappedAction.addPropertyChangeListener(listener);
+  }
+  
+  /**
+   * This passes the call on to the wrapped Action.
+   */
+  public Object getValue(String key) {
+    return wrappedAction.getValue(key);
+  }
+  
+  /**
+   * This passes the call on to the wrapped Action.
+   */
+  public boolean isEnabled() {
+    return wrappedAction.isEnabled();
+  }
+  
+  /**
+   * This passes the call on to the wrapped Action.
+   */
+  public void putValue(String key, Object newValue) {
+    wrappedAction.putValue(key, newValue);
+    // have to call super.putValue() in order to work around a bug in
+    // javax.swing.AbstractAction.clone() later
+    super.putValue(key, newValue);
+  }
+  
+  /**
+   * This passes the call on to the wrapped Action.
+   */
+  public void removePropertyChangeListener(PropertyChangeListener listener) {
+    wrappedAction.removePropertyChangeListener(listener);
+  }
+  
+  /**
+   * This passes the call on to the wrapped Action.
+   */
+  public void setEnabled(boolean newValue) {
+    wrappedAction.setEnabled(newValue);
+  }
+	
+  public Action getWrappedAction() {
+    return wrappedAction;
+  }
+  
+  public void setWrappedAction(Action newValue) {
+    wrappedAction = newValue;
+  }
+  
+  public ActionThread getThread() {
+    return thread;
+  }
+  
+  public void setThread(ActionThread newValue) {
+    thread=newValue;
+  }
+  
+  public Action cloneDynamicAction() throws CloneNotSupportedException {
+    if (wrappedAction instanceof DynamicAbstractAction) {
+      this.putValue("bugWorkaround", "true");
+      ActionWrapper newAction = (ActionWrapper)this.clone();
+      newAction.setWrappedAction(((DynamicAbstractAction) wrappedAction).cloneDynamicAction());
+      return newAction;
+    } else {
+      throw new CloneNotSupportedException();
     }
-	
-	/**
-	 * This performs the wrapped Action on the configured Thread.
-     */
-	public void actionPerformed(ActionEvent e) {
-		thread.addToQueue(wrappedAction, e);
-	}
-	
-	/**
-	 * This passes the call on to the wrapped Action.
-	 */
-	public void addPropertyChangeListener(PropertyChangeListener listener) {
-		wrappedAction.addPropertyChangeListener(listener);
-	}
-	
-	/**
-	 * This passes the call on to the wrapped Action.
-	 */
-	public Object getValue(String key) {
-		return wrappedAction.getValue(key);
-	}
-	
-	/**
-	 * This passes the call on to the wrapped Action.
-	 */
-	public boolean isEnabled() {
-		return wrappedAction.isEnabled();
-	}
-	
-	/**
-	 * This passes the call on to the wrapped Action.
-	 */
-	public void putValue(String key, Object newValue) {
-		wrappedAction.putValue(key, newValue);
-	}
-	
-	/**
-	 * This passes the call on to the wrapped Action.
-	 */
-	public void removePropertyChangeListener(PropertyChangeListener listener) {
-		wrappedAction.removePropertyChangeListener(listener);
-	}
-	
-	/**
-	 * This passes the call on to the wrapped Action.
-	 */
-	public void setEnabled(boolean newValue) {
-		wrappedAction.setEnabled(newValue);
-	}
-	
-	
-	public Action getWrappedAction() {
-		return wrappedAction;
-	}
-	
-	public void setWrappedAction(Action newValue) {
-		wrappedAction = newValue;
-	}
-	
-	public ActionThread getThread() {
-		return thread;
-	}
-	
-	public void setThread(ActionThread newValue) {
-		thread=newValue;
-	}
-
-	public Action cloneDynamicAction() throws CloneNotSupportedException {
-
-		if (wrappedAction instanceof DynamicAbstractAction) {
-			ActionWrapper newAction = (ActionWrapper) this.clone();
-			newAction.setWrappedAction(((DynamicAbstractAction) wrappedAction).cloneDynamicAction());
-			return newAction;
-		} else {
-			throw new CloneNotSupportedException();
-		}
-	}
-
+  }
+  
 }
