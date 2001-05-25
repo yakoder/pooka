4368dca8baef4d447891bf6a18b7d8c9e43fa103
akp <akp@cf05c91d-1d0e-0410-8b41-cf3121da992e>
2001-05-25 19:19:09 +0000

    first draft of a class to model an outgoing message queue.

---
+++ pooka/net/suberic/pooka/OutgoingFolderInfo.java
@@ -0,0 +1,47 @@
+package net.suberic.pooka;
+import javax.mail.*;
+import javax.mail.internet.*;
+
+/**
+ * This class represents a folder which stores outgoing messages until they
+ * can connect to the SMTP server.
+ */
+public class OutgoingFolderInfo extends FolderInfo {
+  URLName transportURL;
+
+  /**
+   * Creates a new OutgoingFolderInfo for the given URLName.
+   */
+  public OutgoingFolderInfo(StoreInfo parent, String fname, URLName outgoingURL) {
+    super(parent, fname);
+    transportURL = outgoingURL;
+  }
+
+  /**
+   * Sends all available messages.
+   */
+  public void sendAll() throws MessagingException {
+
+    Transport sendTransport = Pooka.getDefaultSession().getTransport(transportURL); 
+    try {
+      sendTransport.connect();
+      
+      Message[] msgs = getFolder().getMessages();    
+      
+      try {
+	for (int i = 0; i < msgs.length; i++) {
+	  Message m = msgs[i];
+	  if (! m.isSet(Flags.Flag.DRAFT)) {
+	    sendTransport.sendMessage(m, m.getAllRecipients());
+	    m.setFlag(Flags.Flag.DELETED, true);
+	  }
+	}
+      } finally {
+	getFolder().expunge();
+      }
+    } finally {
+      sendTransport.close();
+    }
+  }
+  
+}
