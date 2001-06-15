68a7593fa41ad21af78d55cbccd9f0f51e89d987
akp <akp@cf05c91d-1d0e-0410-8b41-cf3121da992e>
2001-06-15 14:18:13 +0000

    adding the AddressBook interface.

---
+++ pooka/net/suberic/pooka/AddressBook.java
@@ -0,0 +1,13 @@
+package net.suberic.pooka;
+
+/**
+ * Defines the methods used to store Addresses.
+ */
+public interface AddressBook {
+
+  /**
+   * Gets and appropriate AddressMatcher.
+   */
+  public AddressMatcher getAddressMatcher();
+
+}
