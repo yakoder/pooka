d82e5c16685a01dab73ecdb6f5c1c2f457f0fcd2
akp <akp@cf05c91d-1d0e-0410-8b41-cf3121da992e>
2001-07-23 14:56:01 +0000

    re-tabbed before editing.

--- pooka/net/suberic/pooka/MailQueue.java
+++ pooka/net/suberic/pooka/MailQueue.java
@@ -12,137 +12,137 @@ import java.util.*;
  * running in disconnected mode, this queue won't do.
  */
 public class MailQueue {
-    Hashtable transportTable;
-    Session session;
-
-    ActionThread thread = new ActionThread(Pooka.getProperty("thread.sendMailThread", "Send Mail Thread"));
-
-    public MailQueue(Session newSession) {
-	session = newSession;
-	transportTable = new Hashtable();
-	thread.start();
-    }
-
-    /**
-     * This adds the Message to the queue associated with the given 
-     * transportURL.  If Message.sendImmediately is set to true, then this
-     * will also go ahead and try to send all queued Messages using
-     * sendQueued().
-     *
-     * Note that at the moment, only Message.sendImmediately is supported.
-     */
-    public void sendMessage(NewMessageInfo pNmi, URLName pTransportURL) throws MessagingException {
-	/*
-	Vector transportQueue = (Vector)transportTable.get(transportURL);
-	if (transportQueue == null) {
-	    transportQueue = new Vector();
-	    transportTable.put(transportURL, transportQueue);
-	}
-	
-	transportQueue.add(m);
-	if (Pooka.getProperty("Message.sendImmediately", "false").equals("true")) {
-	    sendQueued();
-	}
-	*/
-
-	final URLName transportURL = pTransportURL;
-	final NewMessageInfo nmi = pNmi;
-
-	if (Pooka.getProperty("Message.sendImmediately", "false").equalsIgnoreCase("true")) {
-	    thread.addToQueue(new net.suberic.util.thread.ActionWrapper(new javax.swing.AbstractAction() {
-		    
-                    public void actionPerformed(java.awt.event.ActionEvent actionEvent) {
-			try {
-			    Transport sendTransport;
-			    
-			    sendTransport = session.getTransport(transportURL); 
-			    sendTransport.connect();
-			    
-			    Message m = nmi.getMessage();
-			    sendTransport.sendMessage(m, m.getAllRecipients());
-			    
-			    javax.swing.SwingUtilities.invokeLater(new Runnable() {
-				    public void run() {
-					net.suberic.pooka.gui.MessageProxy proxy = nmi.getMessageProxy();
-					if (proxy != null) {
-					    net.suberic.pooka.gui.MessageUI mui = proxy.getMessageUI();
-					    if (mui != null)
-						mui.closeMessageUI();
-					}
-				    }
-				});
-
-			} catch (MessagingException me) {
-			    net.suberic.pooka.gui.MessageUI mui = null;
-			    net.suberic.pooka.gui.MessageProxy proxy = nmi.getMessageProxy();
-			    if (proxy != null) {			    
-				mui = proxy.getMessageUI();
-			    }
-			    
-			    if (mui != null)
-				mui.showError(Pooka.getProperty("error.MessageWindow.sendFailed", "Failed to send Message"), me);
-			    else
-				Pooka.getUIFactory().showError(Pooka.getProperty("error.MessageWindow.sendFailed", "Failed to send Message"), me);
-			}
+  Hashtable transportTable;
+  Session session;
+  
+  ActionThread thread = new ActionThread(Pooka.getProperty("thread.sendMailThread", "Send Mail Thread"));
+  
+  public MailQueue(Session newSession) {
+    session = newSession;
+    transportTable = new Hashtable();
+    thread.start();
+  }
+  
+  /**
+   * This adds the Message to the queue associated with the given 
+   * transportURL.  If Message.sendImmediately is set to true, then this
+   * will also go ahead and try to send all queued Messages using
+   * sendQueued().
+   *
+   * Note that at the moment, only Message.sendImmediately is supported.
+   */
+  public void sendMessage(NewMessageInfo pNmi, URLName pTransportURL) throws MessagingException {
+    /*
+      Vector transportQueue = (Vector)transportTable.get(transportURL);
+      if (transportQueue == null) {
+      transportQueue = new Vector();
+      transportTable.put(transportURL, transportQueue);
+      }
+      
+      transportQueue.add(m);
+      if (Pooka.getProperty("Message.sendImmediately", "false").equals("true")) {
+      sendQueued();
+      }
+    */
+    
+    final URLName transportURL = pTransportURL;
+    final NewMessageInfo nmi = pNmi;
+    
+    if (Pooka.getProperty("Message.sendImmediately", "false").equalsIgnoreCase("true")) {
+      thread.addToQueue(new net.suberic.util.thread.ActionWrapper(new javax.swing.AbstractAction() {
+	  
+	  public void actionPerformed(java.awt.event.ActionEvent actionEvent) {
+	    try {
+	      Transport sendTransport;
+	      
+	      sendTransport = session.getTransport(transportURL); 
+	      sendTransport.connect();
+	      
+	      Message m = nmi.getMessage();
+	      sendTransport.sendMessage(m, m.getAllRecipients());
+	      
+	      javax.swing.SwingUtilities.invokeLater(new Runnable() {
+		  public void run() {
+		    net.suberic.pooka.gui.MessageProxy proxy = nmi.getMessageProxy();
+		    if (proxy != null) {
+		      net.suberic.pooka.gui.MessageUI mui = proxy.getMessageUI();
+		      if (mui != null)
+			mui.closeMessageUI();
 		    }
-		}, thread), new java.awt.event.ActionEvent(nmi, 1, "message-send"));
-	    
-	    
-	}	      
-
-    }
-
-    /**
-     * This will try to send all the Messages currently in the queue.  If 
-     * it encounters any major errors (i.e being unable to connect to the
-     * mailserver), it will more or less exit out, throwing a 
-     * MessagingException saying why it failed.  If it manages to send some
-     * Messages but not others, it will send the ones that it can, and return
-     * a MessagingException with all the individual sub-exceptions tacked
-     * on to it using MessagingException.setNextException().
-     *
-     * Note:  do not use this method.  i am now just keeping it here as
-     * a placeholder until i implement a real outgoing message queue.
-     */
-    public void sendQueued() throws MessagingException {
-	Enumeration keys = transportTable.keys();
-	URLName transportURL;
-	Transport sendTransport;
-	Vector sendQueue;
-	Message msg;
-	boolean error = false;
-	
-	while (keys.hasMoreElements()) {
-	    transportURL = (URLName)keys.nextElement();
-	    sendQueue = (Vector)transportTable.get(transportURL);
-	    
-	    if (!(sendQueue.isEmpty())) {
-		MessagingException returnException = null;
-		sendTransport = session.getTransport(transportURL); 
-		sendTransport.connect();
-		
-		for (int i=sendQueue.size()-1 ; i >= 0; i--) {
-		    msg = (Message)sendQueue.elementAt(i);
-		    try {
-			sendTransport.sendMessage(msg, msg.getAllRecipients());
-			sendQueue.removeElementAt(i);
-		    } catch (SendFailedException sfe) {
-			if (returnException == null) 
-			    returnException = sfe;
-			else
-			    returnException.setNextException(sfe);
-		    } catch (MessagingException me) {
-			if (returnException == null)  
-			    returnException = me;
-			else
-			    returnException.setNextException(me);
-		    }
-		}
-		if (returnException != null)
-		    throw returnException;
+		  }
+		});
+	      
+	    } catch (MessagingException me) {
+	      net.suberic.pooka.gui.MessageUI mui = null;
+	      net.suberic.pooka.gui.MessageProxy proxy = nmi.getMessageProxy();
+	      if (proxy != null) {			    
+		mui = proxy.getMessageUI();
+	      }
+	      
+	      if (mui != null)
+		mui.showError(Pooka.getProperty("error.MessageWindow.sendFailed", "Failed to send Message"), me);
+	      else
+		Pooka.getUIFactory().showError(Pooka.getProperty("error.MessageWindow.sendFailed", "Failed to send Message"), me);
 	    }
+	  }
+	}, thread), new java.awt.event.ActionEvent(nmi, 1, "message-send"));
+      
+      
+    }	      
+    
+  }
+  
+  /**
+   * This will try to send all the Messages currently in the queue.  If 
+   * it encounters any major errors (i.e being unable to connect to the
+   * mailserver), it will more or less exit out, throwing a 
+   * MessagingException saying why it failed.  If it manages to send some
+   * Messages but not others, it will send the ones that it can, and return
+   * a MessagingException with all the individual sub-exceptions tacked
+   * on to it using MessagingException.setNextException().
+   *
+   * Note:  do not use this method.  i am now just keeping it here as
+   * a placeholder until i implement a real outgoing message queue.
+   */
+  public void sendQueued() throws MessagingException {
+    Enumeration keys = transportTable.keys();
+    URLName transportURL;
+    Transport sendTransport;
+    Vector sendQueue;
+    Message msg;
+    boolean error = false;
+    
+    while (keys.hasMoreElements()) {
+      transportURL = (URLName)keys.nextElement();
+      sendQueue = (Vector)transportTable.get(transportURL);
+      
+      if (!(sendQueue.isEmpty())) {
+	MessagingException returnException = null;
+	sendTransport = session.getTransport(transportURL); 
+	sendTransport.connect();
+	
+	for (int i=sendQueue.size()-1 ; i >= 0; i--) {
+	  msg = (Message)sendQueue.elementAt(i);
+	  try {
+	    sendTransport.sendMessage(msg, msg.getAllRecipients());
+	    sendQueue.removeElementAt(i);
+	  } catch (SendFailedException sfe) {
+	    if (returnException == null) 
+	      returnException = sfe;
+	    else
+	      returnException.setNextException(sfe);
+	  } catch (MessagingException me) {
+	    if (returnException == null)  
+	      returnException = me;
+	    else
+	      returnException.setNextException(me);
+	  }
 	}
+	if (returnException != null)
+	  throw returnException;
+      }
     }
+  }
 }
     
 
--- pooka/net/suberic/pooka/gui/NewMessageProxy.java
+++ pooka/net/suberic/pooka/gui/NewMessageProxy.java
@@ -13,181 +13,181 @@ import java.io.*;
  * This class represents a new message that is being written.
  */
 public class NewMessageProxy extends MessageProxy {
-    Hashtable commands;
-
-    public NewMessageProxy(NewMessageInfo newMessage) {
-	messageInfo = newMessage;
-
-	commands = new Hashtable();
-	
-        Action[] actions = getActions();
-        if (actions != null) {
-            for (int i = 0; i < actions.length; i++) {
-                Action a = actions[i];
-                commands.put(a.getValue(Action.NAME), a);
-            }
-        }
-	
-    }
-
-    public void openWindow() {
-	// shouldn't have to open window.
-    }
-
-    public void moveMessage(Folder targetFolder) {
-	// shouldn't have to.  might want to implement this to move a message
-	// to drafts, though.
-    }
-
-    /**
-     * This sends the Message associated with this MessageProxy.
-     *
-     * If this MessageProxy has a MessageUI associated with it, it 
-     * will try to load the information from it, and then send the message.
-     * Otherwise, it will just try sending the message as-is.
-     *
-     * If the Message.sendImmediately property is set, then the method will 
-     * also pop up and error window if there are any problems sending the 
-     * queued messages.  
-     *
-     * If there is a MessageUI associated with this Proxy, and either 
-     * there are no errors sending the message, or the Message is just added 
-     * to the Queue and not sent yet, the Window will also be closed.
-     *
-     */
-    public void send() {
-	if (getNewMessageUI() != null) { 
-	    try {
-		UserProfile profile = getNewMessageUI().getSelectedProfile();
-		InternetHeaders headers = getNewMessageUI().getMessageHeaders();
-
-		String messageText = getNewMessageUI().getMessageText();
-
-		String messageContentType = getNewMessageUI().getMessageContentType();
-		getNewMessageInfo().sendMessage(profile, headers, messageText, messageContentType);
-
-		try {
-		    getNewMessageInfo().saveToSentFolder(profile);
-		} catch (MessagingException me) {
-		    getMessageUI().showError(Pooka.getProperty("Error.SaveFile.toSentFolder", "Error saving file to sent folder."), Pooka.getProperty("error.SaveFile.toSentFolder.title", "Error storing message."));
-		}
-		getNewMessageUI().setModified(false);
-		getMessageUI().closeMessageUI();
-	    } catch (MessagingException me) {
-		if (me instanceof SendFailedException) {
-		    getMessageUI().showError(Pooka.getProperty("error.MessageUI.sendFailed", "Failed to send Message.") + "\n" + me.getMessage());
-		    me.printStackTrace(System.out);
-		} else {
-		    getMessageUI().showError(Pooka.getProperty("error.MessageUI.sendFailed", "Failed to send Message.") + "\n" + me.getMessage());
-		    me.printStackTrace(System.out);
-		}
-	    }
-	}
+  Hashtable commands;
+  
+  public NewMessageProxy(NewMessageInfo newMessage) {
+    messageInfo = newMessage;
+    
+    commands = new Hashtable();
+    
+    Action[] actions = getActions();
+    if (actions != null) {
+      for (int i = 0; i < actions.length; i++) {
+	Action a = actions[i];
+	commands.put(a.getValue(Action.NAME), a);
+      }
     }
     
-    /**
-     * This attaches a file to a given message.  Really, all it does is
-     * calls getFileToAttach(), and then sends that to attachFile().
-     */
-    public void attach() {
-	File[] f = getFileToAttach();
-	if (f != null) {
-	    for (int i = 0; i < f.length; i++)
-		attachFile(f[i]);
-	}
-    }
-
-    /**
-     * This calls on the MessageUI to bring up a FileDialog to choose
-     * the file to attach to the message.  If no choice is made, this 
-     * method returns null.
-     */
-    public File[] getFileToAttach() {
-	return getNewMessageUI().getFiles(Pooka.getProperty("MessageUI.attachFileDialog.title", "Choose file to attach."), Pooka.getProperty("MessageUI.attachFileDialog.buttonText", "Attach"));
-    }
-
-    /**
-     * This actually attaches the File to the Message.  Any errors are 
-     * sent to the MessageUI to display.  
-     *
-     * This also sets the 'hasAttachment' property on the MessageUI
-     * to true.
-     */
-    public void attachFile(File f) {
+  }
+  
+  public void openWindow() {
+    // shouldn't have to open window.
+  }
+  
+  public void moveMessage(Folder targetFolder) {
+    // shouldn't have to.  might want to implement this to move a message
+    // to drafts, though.
+  }
+  
+  /**
+   * This sends the Message associated with this MessageProxy.
+   *
+   * If this MessageProxy has a MessageUI associated with it, it 
+   * will try to load the information from it, and then send the message.
+   * Otherwise, it will just try sending the message as-is.
+   *
+   * If the Message.sendImmediately property is set, then the method will 
+   * also pop up and error window if there are any problems sending the 
+   * queued messages.  
+   *
+   * If there is a MessageUI associated with this Proxy, and either 
+   * there are no errors sending the message, or the Message is just added 
+   * to the Queue and not sent yet, the Window will also be closed.
+   *
+   */
+  public void send() {
+    if (getNewMessageUI() != null) { 
+      try {
+	UserProfile profile = getNewMessageUI().getSelectedProfile();
+	InternetHeaders headers = getNewMessageUI().getMessageHeaders();
+	
+	String messageText = getNewMessageUI().getMessageText();
+	
+	String messageContentType = getNewMessageUI().getMessageContentType();
+	getNewMessageInfo().sendMessage(profile, headers, messageText, messageContentType);
+	
 	try {
-	    getNewMessageInfo().attachFile(f);
-
-	    getNewMessageUI().attachmentAdded(getNewMessageInfo().getAttachments().size() -1);
-	} catch (Exception e) {
-	    getMessageUI().showError(Pooka.getProperty("error.MessageUI.unableToAttachFile", "Unable to attach file."), Pooka.getProperty("error.MessageUI.unableToAttachFile.title", "Unable to Attach File."), e);
+	  getNewMessageInfo().saveToSentFolder(profile);
+	} catch (MessagingException me) {
+	  getMessageUI().showError(Pooka.getProperty("Error.SaveFile.toSentFolder", "Error saving file to sent folder."), Pooka.getProperty("error.SaveFile.toSentFolder.title", "Error storing message."));
 	}
-	
-    }
-
-    /**
-     * This removes the given Attachment from the list of attachments.
-     * I figure that you're likely only to be removing attachments from 
-     * the attachment list itself, so you should be able to get the
-     * correct underlying object.
-     */
-    public void detachFile(Attachment a) {
-	int index = getNewMessageInfo().removeAttachment(a);
-	if (index != -1)
-	    getNewMessageUI().attachmentRemoved(index);
-    }
-
-    /**
-     * a convenience method which returns the current MessageUI as
-     * a NewMessageUI.
-     */
-    public NewMessageUI getNewMessageUI() {
-	if (getMessageUI() instanceof NewMessageUI)
-	    return (NewMessageUI)getMessageUI();
-	else
-	    return null;
-    }
-
-    /** 
-     * a convenience method which returns the current MessageInfo as
-     * a NewMessageInfo.
-     */
-    public NewMessageInfo getNewMessageInfo() {
-	return (NewMessageInfo) messageInfo;
-    }
-
-    public Action[] defaultActions = {
-	new SendAction(),
-	new AttachAction()
-	    };
-    
-    public Action getAction(String name) {
-	return (Action)commands.get(name);
-    }
-
-    public Action[] getActions() {
-	return defaultActions;
-    }
-
-    class SendAction extends AbstractAction {
-	SendAction() {
-	    super("message-send");
+	getNewMessageUI().setModified(false);
+	getMessageUI().closeMessageUI();
+      } catch (MessagingException me) {
+	if (me instanceof SendFailedException) {
+	  getMessageUI().showError(Pooka.getProperty("error.MessageUI.sendFailed", "Failed to send Message.") + "\n" + me.getMessage());
+	  me.printStackTrace(System.out);
+	} else {
+	  getMessageUI().showError(Pooka.getProperty("error.MessageUI.sendFailed", "Failed to send Message.") + "\n" + me.getMessage());
+	  me.printStackTrace(System.out);
 	}
-
-	public void actionPerformed(ActionEvent e) {
-	    send();
-	}
+      }
     }
-
-    class AttachAction extends AbstractAction {
-	AttachAction() {
-	    super("message-attach-file");
-	}
-
-	public void actionPerformed(ActionEvent e) {
-	    attach();
-	}
+  }
+  
+  /**
+   * This attaches a file to a given message.  Really, all it does is
+   * calls getFileToAttach(), and then sends that to attachFile().
+   */
+  public void attach() {
+    File[] f = getFileToAttach();
+    if (f != null) {
+      for (int i = 0; i < f.length; i++)
+	attachFile(f[i]);
+    }
+  }
+  
+  /**
+   * This calls on the MessageUI to bring up a FileDialog to choose
+   * the file to attach to the message.  If no choice is made, this 
+   * method returns null.
+   */
+  public File[] getFileToAttach() {
+    return getNewMessageUI().getFiles(Pooka.getProperty("MessageUI.attachFileDialog.title", "Choose file to attach."), Pooka.getProperty("MessageUI.attachFileDialog.buttonText", "Attach"));
+  }
+  
+  /**
+   * This actually attaches the File to the Message.  Any errors are 
+   * sent to the MessageUI to display.  
+   *
+   * This also sets the 'hasAttachment' property on the MessageUI
+   * to true.
+   */
+  public void attachFile(File f) {
+    try {
+      getNewMessageInfo().attachFile(f);
+      
+      getNewMessageUI().attachmentAdded(getNewMessageInfo().getAttachments().size() -1);
+    } catch (Exception e) {
+      getMessageUI().showError(Pooka.getProperty("error.MessageUI.unableToAttachFile", "Unable to attach file."), Pooka.getProperty("error.MessageUI.unableToAttachFile.title", "Unable to Attach File."), e);
     }
     
+  }
+  
+  /**
+   * This removes the given Attachment from the list of attachments.
+   * I figure that you're likely only to be removing attachments from 
+   * the attachment list itself, so you should be able to get the
+   * correct underlying object.
+   */
+  public void detachFile(Attachment a) {
+    int index = getNewMessageInfo().removeAttachment(a);
+    if (index != -1)
+      getNewMessageUI().attachmentRemoved(index);
+  }
+  
+  /**
+   * a convenience method which returns the current MessageUI as
+   * a NewMessageUI.
+   */
+  public NewMessageUI getNewMessageUI() {
+    if (getMessageUI() instanceof NewMessageUI)
+      return (NewMessageUI)getMessageUI();
+    else
+      return null;
+  }
+  
+  /** 
+   * a convenience method which returns the current MessageInfo as
+   * a NewMessageInfo.
+   */
+  public NewMessageInfo getNewMessageInfo() {
+    return (NewMessageInfo) messageInfo;
+  }
+  
+  public Action[] defaultActions = {
+    new SendAction(),
+    new AttachAction()
+      };
+  
+  public Action getAction(String name) {
+    return (Action)commands.get(name);
+  }
+
+  public Action[] getActions() {
+    return defaultActions;
+  }
+  
+  class SendAction extends AbstractAction {
+    SendAction() {
+      super("message-send");
+    }
+    
+    public void actionPerformed(ActionEvent e) {
+      send();
+    }
+  }
+  
+  class AttachAction extends AbstractAction {
+    AttachAction() {
+      super("message-attach-file");
+    }
+    
+    public void actionPerformed(ActionEvent e) {
+      attach();
+    }
+  }
+  
 }
 
 
