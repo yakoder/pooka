085da523e01d95ec432a5ac73fce6b4d333c2b21
akp <akp@cf05c91d-1d0e-0410-8b41-cf3121da992e>
2001-10-08 19:07:06 +0000

    just adding a bit more.  i'll need to figure out just how the 'online' flag works.

--- pooka/net/suberic/pooka/OutgoingFolderInfo.java
+++ pooka/net/suberic/pooka/OutgoingFolderInfo.java
@@ -9,6 +9,7 @@ import javax.mail.internet.*;
 public class OutgoingFolderInfo extends FolderInfo {
   URLName transportURL;
   Thread sendMailThread = new net.suberic.util.thread.ActionThread(Pooka.getProperty("SendMailThread.name", "Send Mail Thread"));
+  boolean online = true;
 
   /**
    * Creates a new OutgoingFolderInfo for the given URLName.
@@ -46,9 +47,40 @@ public class OutgoingFolderInfo extends FolderInfo {
   }
 
   /**
-   * Virtually sends a message.
+   * Virtually sends a message.  If the current status is connected, then
+   * the message will actually be sent now.  If not, and the 
+   * Message.sendImmediately setting is true, then we'll attempt to send
+   * the message anyway.  
+   */
+  public void sendMessage(NewMessageInfo nmi, boolean connect) throws javax.mail.MessagingException {
+    this.appendMessages(new MessageInfo[] { nmi });
+    if (online)
+      sendAll();
+    else if (connect || Pooka.getProperty("Message.sendImmediately", "false").equalsIgnoreCase("true")) {
+      try {
+	connect();
+	if (online)
+	  sendAll();
+      } catch (MessagingException me) {
+	System.err.println("me is a " + me);
+	online = false;
+      }
+    }
+  }
+
+  /**
+   * Virtually sends a message.  If the current status is connected, then
+   * the message will actually be sent now.  
    */
   public void sendMessage(NewMessageInfo nmi) throws javax.mail.MessagingException {
-    this.appendMessages(new MessageInfo[] { nmi });
+    sendMessage(nmi, false);
+  }
+
+  /**
+   * Attempts to connect this OutgoingFolderInfo.
+   */
+  public void connect() {
+    // does nothing right now.
+    online=true;
   }
 }
