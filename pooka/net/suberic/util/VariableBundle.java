a25e6309c19bd713c3917c7d583ba0d93923f6c3
akp <akp@cf05c91d-1d0e-0410-8b41-cf3121da992e>
2002-03-04 21:08:53 +0000

    fixed up the VariableBundle to handle special characters ( '\' primarily ) and whitespace a bit better.

--- pooka/net/suberic/util/VariableBundle.java
+++ pooka/net/suberic/util/VariableBundle.java
@@ -192,80 +192,185 @@ public class VariableBundle extends Object {
      * VariableBundle--underlying defaults are not written.
      */
     public void saveProperties(File saveFile) {
-	if (writableProperties.size() > 0) { 
-	    File outputFile;
-	    String currentLine, key;
-	    int equalsLoc;
+      if (writableProperties.size() > 0) { 
+	File outputFile;
+	String currentLine, key;
+	int equalsLoc;
+	
+	try {
+	  if (!saveFile.exists())
+	    saveFile.createNewFile();
+	  
+	  outputFile  = saveFile.createTempFile(saveFile.getName(), ".tmp", saveFile.getParentFile());
+	  
+	  BufferedReader readSaveFile = new BufferedReader(new FileReader(saveFile));
+	  BufferedWriter writeSaveFile = new BufferedWriter(new FileWriter(outputFile));
+	  currentLine = readSaveFile.readLine();
+	  while (currentLine != null) {
+	    equalsLoc = currentLine.indexOf('=');
+	    if (equalsLoc != -1) {
+	      String rawKey = currentLine.substring(0, equalsLoc);
+	      key = unEscapeString(rawKey);
 
-	    try {
-		if (!saveFile.exists())
-		    saveFile.createNewFile();
+	      if (!propertyIsRemoved(key)) {
+		if (writableProperties.getProperty(key, "").equals("")) {
 
-		outputFile  = saveFile.createTempFile(saveFile.getName(), ".tmp", saveFile.getParentFile());
+		  writeSaveFile.write(currentLine);
+		  writeSaveFile.newLine();
 
-		BufferedReader readSaveFile = new BufferedReader(new FileReader(saveFile));
-		BufferedWriter writeSaveFile = new BufferedWriter(new FileWriter(outputFile));
-		currentLine = readSaveFile.readLine();
-		while (currentLine != null) {
-		    equalsLoc = currentLine.indexOf('=');
-		    if (equalsLoc != -1) {
-			key = currentLine.substring(0, equalsLoc);
-			if (!propertyIsRemoved(key)) {
-			    if (writableProperties.getProperty(key, "").equals("")) {
-				writeSaveFile.write(currentLine);
-				writeSaveFile.newLine();
-				
-			    } else {
-				writeSaveFile.write(key + "=" + writableProperties.getProperty(key, ""));
-				writeSaveFile.newLine();
-				properties.setProperty(key, writableProperties.getProperty(key, ""));
-				writableProperties.remove(key);
-			    }
-			    removeProperty(key);
-			}
-			
-		    } else {
-			writeSaveFile.write(currentLine);
-			writeSaveFile.newLine();
-		    }
-		    currentLine = readSaveFile.readLine();
+		} else {
+		  writeSaveFile.write(rawKey + "=" + escapeWhiteSpace(writableProperties.getProperty(key, "")));
+		  writeSaveFile.newLine();
+		  properties.setProperty(key, writableProperties.getProperty(key, ""));
+		  writableProperties.remove(key);
 		}
-
-		// write out the rest of the writableProperties
-
-		Enumeration propsLeft = writableProperties.keys();
-		while (propsLeft.hasMoreElements()) {
-		    String nextKey = (String)propsLeft.nextElement();
-		    String nextKeyEscaped = escapeWhiteSpace(nextKey);
-		    writeSaveFile.write(nextKeyEscaped + "=" + writableProperties.getProperty(nextKey, ""));
-		    writeSaveFile.newLine();
-		    properties.setProperty(nextKey, writableProperties.getProperty(nextKey, ""));
-		    writableProperties.remove(nextKey);
-		}
-		
-		clearRemoveList();
-
-		readSaveFile.close();
-		writeSaveFile.flush();
-		writeSaveFile.close();
-
-                // if you don't delete the .old file first, then the
-                // rename fails under Windows.
-                String oldSaveName = saveFile.getAbsolutePath() + ".old";
-                File oldSave = new File (oldSaveName);
-                if (oldSave.exists())
-                  oldSave.delete();
-
-		String fileName = new String(saveFile.getAbsolutePath());
-		saveFile.renameTo(oldSave);
-		outputFile.renameTo(new File(fileName));
-		
-	    } catch (Exception e) {
-		System.out.println(getProperty("VariableBundle.saveError", "Error saving properties file: " + saveFile.getName() + ": " + e.getMessage()));
-		e.printStackTrace(System.err);
+		removeProperty(key);
+	      }
+	      
+	    } else {
+	      writeSaveFile.write(currentLine);
+	      writeSaveFile.newLine();
 	    }
-
+	    currentLine = readSaveFile.readLine();
+	  }
+	  
+	  // write out the rest of the writableProperties
+	  
+	  Enumeration propsLeft = writableProperties.keys();
+	  while (propsLeft.hasMoreElements()) {
+	    String nextKey = (String)propsLeft.nextElement();
+	    String nextKeyEscaped = escapeWhiteSpace(nextKey);
+	    String nextValueEscaped = escapeWhiteSpace(writableProperties.getProperty(nextKey, ""));
+	    writeSaveFile.write(nextKeyEscaped + "=" + nextValueEscaped);
+	    writeSaveFile.newLine();
+	    
+	    properties.setProperty(nextKey, writableProperties.getProperty(nextKey, ""));
+	    writableProperties.remove(nextKey);
+	  }
+	  
+	  clearRemoveList();
+	  
+	  readSaveFile.close();
+	  writeSaveFile.flush();
+	  writeSaveFile.close();
+	  
+	  // if you don't delete the .old file first, then the
+	  // rename fails under Windows.
+	  String oldSaveName = saveFile.getAbsolutePath() + ".old";
+	  File oldSave = new File (oldSaveName);
+	  if (oldSave.exists())
+	    oldSave.delete();
+	  
+	  String fileName = new String(saveFile.getAbsolutePath());
+	  saveFile.renameTo(oldSave);
+	  outputFile.renameTo(new File(fileName));
+	  
+	} catch (Exception e) {
+	  System.out.println(getProperty("VariableBundle.saveError", "Error saving properties file: " + saveFile.getName() + ": " + e.getMessage()));
+	  e.printStackTrace(System.err);
 	}
+	
+      }
+    }
+
+    /*
+     * Converts encoded &#92;uxxxx to unicode chars
+     * and changes special saved chars to their original forms
+     *
+     * ripped directly from java.util.Properties; hope they don't mind.
+     */
+    private String loadConvert (String theString) {
+        char aChar;
+        int len = theString.length();
+        StringBuffer outBuffer = new StringBuffer(len);
+
+        for(int x=0; x<len; ) {
+            aChar = theString.charAt(x++);
+            if (aChar == '\\') {
+                aChar = theString.charAt(x++);
+                if(aChar == 'u') {
+                    // Read the xxxx
+                    int value=0;
+		    for (int i=0; i<4; i++) {
+		        aChar = theString.charAt(x++);
+		        switch (aChar) {
+		          case '0': case '1': case '2': case '3': case '4':
+		          case '5': case '6': case '7': case '8': case '9':
+		             value = (value << 4) + aChar - '0';
+			     break;
+			  case 'a': case 'b': case 'c':
+                          case 'd': case 'e': case 'f':
+			     value = (value << 4) + 10 + aChar - 'a';
+			     break;
+			  case 'A': case 'B': case 'C':
+                          case 'D': case 'E': case 'F':
+			     value = (value << 4) + 10 + aChar - 'A';
+			     break;
+			  default:
+                              throw new IllegalArgumentException(
+                                           "Malformed \\uxxxx encoding.");
+                        }
+                    }
+                    outBuffer.append((char)value);
+                } else {
+                    if (aChar == 't') aChar = '\t';
+                    else if (aChar == 'r') aChar = '\r';
+                    else if (aChar == 'n') aChar = '\n';
+                    else if (aChar == 'f') aChar = '\f';
+                    outBuffer.append(aChar);
+                }
+            } else
+                outBuffer.append(aChar);
+        }
+        return outBuffer.toString();
+    }
+
+    /*
+     * Converts unicodes to encoded &#92;uxxxx
+     * and writes out any of the characters in specialSaveChars
+     * with a preceding slash
+     *
+     * ripped directly from java.util.Properties; hope they don't mind.
+     */
+    private String saveConvert(String theString, boolean escapeSpace) {
+        int len = theString.length();
+        StringBuffer outBuffer = new StringBuffer(len*2);
+
+        for(int x=0; x<len; x++) {
+            char aChar = theString.charAt(x);
+            switch(aChar) {
+		case ' ':
+		    if (x == 0 || escapeSpace) 
+			outBuffer.append('\\');
+
+		    outBuffer.append(' ');
+		    break;
+                case '\\':outBuffer.append('\\'); outBuffer.append('\\');
+                          break;
+                case '\t':outBuffer.append('\\'); outBuffer.append('t');
+                          break;
+                case '\n':outBuffer.append('\\'); outBuffer.append('n');
+                          break;
+                case '\r':outBuffer.append('\\'); outBuffer.append('r');
+                          break;
+                case '\f':outBuffer.append('\\'); outBuffer.append('f');
+                          break;
+                default:
+                    if ((aChar < 0x0020) || (aChar > 0x007e)) {
+                        outBuffer.append('\\');
+                        outBuffer.append('u');
+                        outBuffer.append(toHex((aChar >> 12) & 0xF));
+                        outBuffer.append(toHex((aChar >>  8) & 0xF));
+                        outBuffer.append(toHex((aChar >>  4) & 0xF));
+                        outBuffer.append(toHex( aChar        & 0xF));
+                    } else {
+                        if (specialSaveChars.indexOf(aChar) != -1)
+                            outBuffer.append('\\');
+                        outBuffer.append(aChar);
+                    }
+            }
+        }
+        return outBuffer.toString();
     }
 
     /**
@@ -273,17 +378,28 @@ public class VariableBundle extends Object {
      * whitespace character.
      */
     public String escapeWhiteSpace(String sourceString) {
+      /*
 	char[] origString = sourceString.toCharArray();
 	StringBuffer returnString = new StringBuffer();
 	for (int i = 0; i < origString.length; i++) {
 	    char currentChar = origString[i];
-	    if (Character.isWhitespace(currentChar))
-		returnString.append('\\');
+	    if (Character.isWhitespace(currentChar) || '\\' == currentChar)
+	      returnString.append('\\');
+	      
 	    returnString.append(currentChar);
 	}
 
 	return returnString.toString();
+      */
+      return saveConvert(sourceString, true);
     }
+
+  /**
+   * resolves a whitespace-escaped string.
+   */
+  public String unEscapeString(String sourceString) {
+    return loadConvert(sourceString);
+  }
 
     /**
      * Clears the removeList.  This should generally be called after
@@ -419,7 +535,27 @@ public class VariableBundle extends Object {
 	  currentListenerList.remove(vcl);
       }
     }
-	
+
+  /**
+   * Convert a nibble to a hex character
+   * @param	nibble	the nibble to convert.
+   */
+  private static char toHex(int nibble) {
+    return hexDigit[(nibble & 0xF)];
+  }
+  
+  /** A table of hex digits */
+  private static final char[] hexDigit = {
+    '0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'
+  };
+
+  private static final String keyValueSeparators = "=: \t\r\n\f";
+  
+  private static final String strictKeyValueSeparators = "=:";
+
+  private static final String specialSaveChars = "=: \t\r\n\f#!";
+  
+  private static final String whiteSpaceChars = " \t\r\n\f";
 }
 
 
