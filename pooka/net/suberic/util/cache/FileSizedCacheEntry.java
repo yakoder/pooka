0ca43d547b3639de93f1a3962807ff0b2ceb6eb9
akp <akp@cf05c91d-1d0e-0410-8b41-cf3121da992e>
2002-04-03 20:17:58 +0000

    still working on these.   at least they compile.  :)

--- pooka/net/suberic/util/cache/FileSizedCacheEntry.java
+++ pooka/net/suberic/util/cache/FileSizedCacheEntry.java
@@ -1,10 +1,13 @@
 package net.suberic.util.cache;
+import java.io.*;
 
 /**
  * This represents a cached object.  It stores the object itself, plus
  * the size and last accessed time.
+ *
+ * This implementation uses files to store the information in the cache.
  */
-public class SizedCacheEntry {
+public class FileSizedCacheEntry extends SizedCacheEntry {
 
   // the stored object itself.  note that this could be a reference to
   // another object, or a source from which the object may be accessed.
@@ -17,19 +20,17 @@ public class SizedCacheEntry {
   protected long size = 0;
 
   /**
-   * Creates a new, empty SizedCacheEntry.
-   */
-  public SizedCacheEntry() {
-  }
-
-  /**
-   * Creates a new SizedCacheEntry containing value which has been most
+   * Creates a new FileSizedCacheEntry containing value which has been most
    * recently accessed now.
    */
-  public SizedCacheEntry(Object value) {
-    cachedValue = value;
-    // yeah, whatever...
-    size = value.toString().length();
+  public FileSizedCacheEntry(Object value, boolean create, String filename) {
+    File f = new File(filename);
+    try {
+      saveValue(f, value);
+    } catch (Exception e) {
+    }
+    cachedValue = f;
+
     touchEntry();
   }
 
@@ -37,8 +38,44 @@ public class SizedCacheEntry {
    * This gets the cached value.  Implementations may vary for this.
    */
   public Object getCachedValue() {
-    return cachedValue;
+    Object o = null;
+    try {
+      o = loadValue();
+    } catch (IOException ioe) {
+    }
+
+    return o;
   }
+
+  /**
+   * Loads the given value from the source file.
+   *
+   * Note that this should also set the size.
+   */
+  public Object loadValue() throws IOException {
+    File f = getCacheFile();
+    ObjectInputStream ois = new ObjectInputStream(new FileInputStream(f));
+    size = ois.available();
+    Object returnValue = null;
+    try {
+      returnValue = ois.readObject();
+    } catch (ClassNotFoundException cnfe) {
+      throw new IOException("Class not found:  " + cnfe);
+    }
+    ois.close();
+    return returnValue;
+  }
+
+  /**
+   * Saves the given value to the source file.
+   *
+   * Note that this should also set the size.
+   */
+  public void saveValue(File f, Object value) throws IOException {
+    ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(f));
+    oos.writeObject(value);
+    oos.close();
+  } 
 
   /**
    * Deletes this entry.  Should be called in order to clean up entries
@@ -48,7 +85,11 @@ public class SizedCacheEntry {
    * this method if cleanup is required.
    */
   public boolean removeFromCache() {
-    return true;
+    if (cachedValue instanceof File) {
+      ((File) cachedValue).delete();
+      return true;
+    } else
+      return false;
   }
 
   /**
@@ -56,6 +97,7 @@ public class SizedCacheEntry {
    */
   public void touchEntry() {
     lastAccessedTime = System.currentTimeMillis();
+    getCacheFile().setLastModified(lastAccessedTime);
   }
 
   /**
@@ -70,6 +112,13 @@ public class SizedCacheEntry {
    */
   public long getLastAccessedTime() {
     return lastAccessedTime;
+  }
+
+  /**
+   * Gets the File object for the cache.
+   */
+  public File getCacheFile() {
+    return (File) cachedValue;
   }
 
   /**
