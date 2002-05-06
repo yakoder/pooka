ff484c0042fdb4b5fa6baf20f72b12ec4c061018
akp <akp@cf05c91d-1d0e-0410-8b41-cf3121da992e>
2002-05-06 14:49:16 +0000

    checking in change to the ActionThread.

--- pooka/net/suberic/util/thread/ActionThread.java
+++ pooka/net/suberic/util/thread/ActionThread.java
@@ -6,7 +6,7 @@ import java.util.Vector;
 
 /**
  * This is a Thread which handles ActionEvents.  Instead of having the 
- * Action handled by the main thread, instead it is put into a queue on 
+ * Action handled by the main thread, it is put into a queue on 
  * this thread, which will then handle the Action.
  */
 public class ActionThread extends Thread {
@@ -14,18 +14,46 @@ public class ActionThread extends Thread {
   boolean stopMe = false;
 
   Object runLock = new Object();
-  
+
+  // indicates high priority
+  public static int PRIORITY_HIGH = 10;
+
+  // indicates normal priority
+  public static int PRIORITY_NORMAL = 5;
+
+  // indicates low priority
+  public static int PRIORITY_LOW = 0;
+
+  /**
+   * Creates an ActionThread with the given threadName.
+   */
   public ActionThread(String threadName) {
     super(threadName);
   }
   
+  /**
+   * Represents an action/event pair.  This also stores the priority of 
+   * the event.
+   */
   public class ActionEventPair {
     public Action action;
     public ActionEvent event;
-    
+    public int priority = PRIORITY_NORMAL;
+
+    /**
+     * Creates an NORMAL priority ActionEventPair.
+     */
     public ActionEventPair(Action newAction, ActionEvent newEvent) {
+      this(newAction, newEvent, PRIORITY_NORMAL);
+    }
+
+    /**
+     * Creates an ActionEventPair with the given priority.
+     */
+    public ActionEventPair(Action newAction, ActionEvent newEvent, int newPriority) {
       action=newAction;
       event=newEvent;
+      priority = newPriority;
     }
   }
   
@@ -76,7 +104,25 @@ public class ActionThread extends Thread {
    * not already running.
    */
   public synchronized void addToQueue(Action action, ActionEvent event) {
-    actionQueue.addElement(new ActionEventPair(action, event));
+    addToQueue(action, event, PRIORITY_HIGH);
+  } 	
+  
+  /**
+   * This adds an item to the queue.  It also starts up the Thread if it's
+   * not already running.
+   */
+  public synchronized void addToQueue(Action action, ActionEvent event, int priority) {
+    // see where this should go.
+    int index = 0;
+    boolean found = false;
+    while (! found && index < actionQueue.size()) {
+      ActionEventPair current = (ActionEventPair) actionQueue.elementAt(index);
+      if (current.priority < priority)
+	found = true;
+      else
+	index++;
+    }
+    actionQueue.add(index, new ActionEventPair(action, event, priority));
     if (sleeping)
       this.interrupt();
   } 	
