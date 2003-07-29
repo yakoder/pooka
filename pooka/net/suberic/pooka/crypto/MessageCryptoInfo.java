870ddad91ffb46c19c36e60d0d5498783a6cd841
akp <akp@cf05c91d-1d0e-0410-8b41-cf3121da992e>
2003-07-29 21:52:43 +0000

    more crypto updates.

--- pooka/net/suberic/pooka/crypto/MessageCryptoInfo.java
+++ pooka/net/suberic/pooka/crypto/MessageCryptoInfo.java
@@ -12,6 +12,15 @@ public class MessageCryptoInfo {
   // the MessageInfo that we're analyzing.
   MessageInfo msgInfo;
 
+  // whether we've checked the signature yet
+  boolean mCheckedSignature = false;
+
+  // whether we've tried decrypting the message yet.
+  boolean mCheckedDecryption = false;
+  
+  // whether the signature matches or not
+  boolean mSignatureValid = false;
+
   /**
    * Creates a MessageCryptoInfo for this given Message.
    */
@@ -35,4 +44,51 @@ public class MessageCryptoInfo {
     return Pooka.getCryptoManager().getDefaultEncryptionUtils().isEncrypted(msgInfo.getMessage());
   }
 
+  /**
+   * Returns whether or not this message has had its signature checked.
+   * Returns false if the message is not signed in the first place.
+   */
+  public boolean hasCheckedSignature() throws MessagingException {
+    if (! isSigned())
+      return false;
+
+    return mCheckedSignature;
+  }
+
+  /**
+   * Returns whether or not this message has had a decryption attempt.
+   * Returns false if the message is not encrypted in the first place.
+   */
+  public boolean hasTriedDecryption() throws MessagingException {
+    if (! isEncrypted())
+      return false;
+
+    return mCheckedDecryption;
+  }
+
+  /**
+   * Returns whether or not the signature is valid.  If the signature has not
+   * been checked yet, returns false.
+   */
+  public boolean isSignatureValid() throws MessagingException {
+    if (hasCheckedSignature())
+      return mSignatureValid;
+    else
+      return false;
+  }
+
+  /**
+   * Returns whether or not the signature is valid.  If <code>recheck</code>
+   * is set to <code>true</code>, then checks again with the latest keys.
+   */
+  public boolean checkSignature(EncryptionKey key, boolean recheck) throws EncryptionException, MessagingException, java.io.IOException {
+    if (recheck || ! hasCheckedSignature()) {
+      EncryptionUtils cryptoUtils = Pooka.getCryptoManager().getDefaultEncryptionUtils();
+      mSignatureValid =  cryptoUtils.checkSignature((MimeMessage)msgInfo.getMessage(), key);
+      mCheckedSignature = true;
+    }
+
+    return mSignatureValid;
+  }
+  
 }
