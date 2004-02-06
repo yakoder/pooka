ef06dfd670ba8e1b0f056ab5b4ba712067b96ce2
akp <akp@cf05c91d-1d0e-0410-8b41-cf3121da992e>
2004-02-06 22:57:05 +0000

    moving source over to src/ subdirectory.

--- pooka/net/suberic/pooka/NetworkConnectionListener.java
+++
@@ -1,9 +0,0 @@
-package net.suberic.pooka;
-
-public interface NetworkConnectionListener {
-  /**
-   * Notifies this component that the state of a network connection has
-   * changed.
-   */
-  public void connectionStatusChanged(NetworkConnection connection, int newStatus);
-}
