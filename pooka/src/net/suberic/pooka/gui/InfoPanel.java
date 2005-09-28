ddb4f96025f19effb12d090ee3cdf43ece63e6d1
akp <akp@cf05c91d-1d0e-0410-8b41-cf3121da992e>
2005-09-28 13:47:05 +0000

    added a clearing thread for the InfoPanel.

--- pooka/src/net/suberic/pooka/gui/InfoPanel.java
+++ pooka/src/net/suberic/pooka/gui/InfoPanel.java
@@ -4,47 +4,105 @@ import javax.swing.*;
 public class InfoPanel extends JPanel {
   JLabel currentLabel = null;
   ConnectionMonitor monitor = null;
-    public InfoPanel() {
-      //super(new java.awt.FlowLayout(java.awt.FlowLayout.LEFT));
-      super(new java.awt.BorderLayout());
-      currentLabel = new JLabel();
-      this.add(currentLabel, java.awt.BorderLayout.WEST);
-      
-      monitor = new ConnectionMonitor();
-      monitor.monitorConnectionManager(net.suberic.pooka.Pooka.getConnectionManager());
+  Thread panelTimer;
+  long messageTimeout = 30000;
+  long clearTime = -1;
+  boolean cleared= true;
+  boolean exitThread = false;
 
-      this.add(monitor,java.awt.BorderLayout.EAST);
-      
-      javax.swing.border.Border border = BorderFactory.createLoweredBevelBorder();
-      
-      this.setBorder(border);
-      this.getInsets().top=0;
-      this.getInsets().bottom=0;
-      this.setMinimumSize(getPreferredSize());
-    }
-  
-    public void setMessage(String newMessage) {
-	final String msg = newMessage;
-	Runnable runMe = new Runnable() {
-	    public void run() {
-	      currentLabel.setText(msg);
-	      currentLabel.repaint();
-	    }
-	  };
-
-	if (SwingUtilities.isEventDispatchThread()) {
-	  runMe.run();
-	} else {
-	  SwingUtilities.invokeLater(runMe);
+  /**
+   * Creates an InfoPanel and adds a ConnectionMonitor 
+   */
+  public InfoPanel() {
+    //super(new java.awt.FlowLayout(java.awt.FlowLayout.LEFT));
+    super(new java.awt.BorderLayout());
+    currentLabel = new JLabel();
+    this.add(currentLabel, java.awt.BorderLayout.WEST);
+    
+    monitor = new ConnectionMonitor();
+    monitor.monitorConnectionManager(net.suberic.pooka.Pooka.getConnectionManager());
+    
+    this.add(monitor,java.awt.BorderLayout.EAST);
+    
+    javax.swing.border.Border border = BorderFactory.createLoweredBevelBorder();
+    
+    this.setBorder(border);
+    this.getInsets().top=0;
+    this.getInsets().bottom=0;
+    this.setMinimumSize(getPreferredSize());
+    
+    panelTimer = new Thread(new Runnable() {
+	public void run() {
+	  waitForTimeout();
 	}
+      });
+    panelTimer.start();
+  }
+  
+  /**
+   * Sets the message for the InfoPanel.
+   */
+  public void setMessage(String newMessage) {
+    final String msg = newMessage;
+    Runnable runMe = new Runnable() {
+	public void run() {
+	  currentLabel.setText(msg);
+	  currentLabel.repaint();
+	  clearTime = System.currentTimeMillis() + messageTimeout;
+	  cleared = false;
+	  panelTimer.interrupt();
+	}
+      };
+    
+    if (SwingUtilities.isEventDispatchThread()) {
+      runMe.run();
+    } else {
+      SwingUtilities.invokeLater(runMe);
     }
+  }
+  
+  /**
+   * Clears the InfoPanel.
+   */
+  public void clear() {
+    SwingUtilities.invokeLater(new Runnable() {
+	public void run() {
+	  currentLabel.setText("");
+	  currentLabel.repaint();
+	  cleared = true;
+	}
+      });
+  }
+  
+  /**
+   * Stops the panel thread.
+   */
+  public void stopThread() {
+    exitThread = true;
+    panelTimer.interrupt();
+  }
 
-    public void clear() {
-	SwingUtilities.invokeLater(new Runnable() {
-		public void run() {
-		    currentLabel.setText("");
-		    currentLabel.repaint();
-		}
-	    });
-    }
+  /**
+   * Waits for the timeout given and then clears the InfoPanel.
+   */
+  void waitForTimeout() {
+    while (! exitThread) {
+      long currentTime = System.currentTimeMillis();
+      long sleepTime = 30000;
+      if (currentTime >= clearTime) {
+	if (! cleared) {
+	  clear();
+	}
+      } else {
+	sleepTime = clearTime - currentTime;
+      }
+
+      try {
+	Thread.currentThread().sleep(sleepTime);
+      } catch (InterruptedException ie) {
+
+      }
+    }      
+  }
+
 }
