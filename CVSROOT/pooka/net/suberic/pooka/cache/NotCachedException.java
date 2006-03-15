61245bed149cef1f6a687749a3974290fe421664
akp <akp@cf05c91d-1d0e-0410-8b41-cf3121da992e>
2006-03-15 22:14:17 +0000

    removing cvsroot.

--- CVSROOT/pooka/net/suberic/pooka/cache/NotCachedException.java
+++
@@ -1,8 +0,0 @@
-package net.suberic.pooka.cache;
-import javax.mail.MessagingException;
-
-public class NotCachedException extends MessagingException {
-    public NotCachedException(String msg) {
-	super(msg);
-    }
-}
--- CVSROOT/pooka/net/suberic/util/ValueChangeListener.java
+++
@@ -1,5 +0,0 @@
-package net.suberic.util;
-
-public interface ValueChangeListener {
-    public void valueChanged(String changedValue);
-}
